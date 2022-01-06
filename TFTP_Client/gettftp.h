98bf20427f6f939024dbb553b983de2b99067213
ensea <ensea@StudentLab>
2022-01-06 10:56:30 +0100

    First commit

---
+++ TFTP_Client/gettftp.h
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int, char* []);
+void DISPLAY_IP(struct addrinfo);
---
+++ TFTP_Client/puttftp.c
@@ -0,0 +1,6 @@
+#include "gettftp.h"
+
+int main(int argc, char* argv[]){
+	printf("PUT \n");
+	return 0;
+}
